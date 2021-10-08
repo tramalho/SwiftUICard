0aab90216ed6ce779e128555571aee883f306fcd
tramalho <thiagofacul@gmail.com>
2021-10-07 22:45:28 -0300

    init

---
+++ SwiftUICard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICard
+//
+//  Created by Thiago Antonio Ramalho on 07/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICard/SwiftUICardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICardApp.swift
+//  SwiftUICard
+//
+//  Created by Thiago Antonio Ramalho on 07/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
